8421be61a0687a64760169af442f232785bbcff9
woobin0531 <cdevfrr@naver.com>
2025-05-07 16:11:41 +0900

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test{
+	public static void main(String[]args) {
+		final double PI = 3.14;
+		
+		double radius = 10.0;
+		double circleArea = radius*radius*PI;
+		System.out.println("원의 면적 = " + circleArea);
+	}
+}
